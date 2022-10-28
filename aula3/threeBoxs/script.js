618503f9a5f9388d4fe37014faeaae134da9ee08
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2022-10-28 02:27:17 -0300

    ok

--- aula3/threeBoxs/script.js
+++ aula3/threeBoxs/script.js
@@ -3,10 +3,16 @@ var pink1 = document.getElementsByClassName('button1')[0]
 var yellow2 = document.getElementsByClassName('button2')[0]
 var green3 = document.getElementsByClassName('button3')[0]
 
-pink1.addEventListener('click', rosa)
-yellow2.addEventListener('click', amarelo)
-green3.addEventListener('click', purple)
+pink1.addEventListener('mouseenter', rosa)
+pink1.addEventListener('mouseleave', black)
+yellow2.addEventListener('mouseenter', amarelo)
+yellow2.addEventListener('mouseleave', black)
+green3.addEventListener('mouseenter', purple)
+green3.addEventListener('mouseleave', black)
 
+function black(){
+    body.style.backgroundColor= 'black'
+}
 function rosa() {
     body.style.backgroundColor= 'pink'
 }
