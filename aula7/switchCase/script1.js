6dc0f95f166f762987001c0137a9c07d30f6392f
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2022-11-01 17:01:46 -0300

    aula7

---
+++ aula7/switchCase/script1.js
@@ -0,0 +1,96 @@
+var print = document.getElementsByTagName('h2')[0]
+
+
+function signo() {
+    let date = document.getElementsByTagName('input')[0].valueAsDate
+    let day = date.getUTCDate()
+    let month = date.getUTCMonth() + 1
+
+    switch (month) {
+        case 1:
+            if (day <= 20) {
+                print.innerHTML = "Você é capricórnio.";
+            } else if (day >= 21) {
+                print.innerHTML = "Você é aquário.";
+            }
+        case 2:
+            if (day <= 19) {
+                print.innerHTML = "Você é aquário.";
+            } else if (day >= 20) {
+                print.innerHTML = "Você é peixes.";
+            }
+        case 3:
+            if (day <= 20) {
+                print.innerHTML = "Você é peixes.";
+            } else if (day >= 21) {
+                print.innerHTML = "Você é áries.";
+            }
+        case 4:
+            if (day <= 20) {
+                print.innerHTML = "Você é áries.";
+            }
+            else if (day >= 21) {
+                print.innerHTML = "Você é touro.";
+            }
+            break
+        case 5:
+            if (day <= 20) {
+                print.innerHTML = "Você é touro.";
+            }
+            else if (day >= 21) {
+                print.innerHTML = "Você é gêmeos.";
+            }
+        case 6:
+            if (day <= 20) {
+                print.innerHTML = "Você é gêmeos.";
+            }
+            else if (day >= 21) {
+                print.innerHTML = "Você é câncer.";
+            }
+        case 7:
+            if (day <= 22) {
+                print.innerHTML = "Você é câncer.";
+            }
+            else if (day >= 23) {
+                print.innerHTML = "Você é leão.";
+            }
+        case 8:
+            if (day <= 22) {
+                print.innerHTML = "Você é leão.";
+            }
+            else if (day >= 23) {
+                print.innerHTML = "Você é virgem.";
+            }
+        case 9:
+            if (day <= 22) {
+                print.innerHTML = "Você é virgem.";
+            }
+            else if (day >= 23) {
+                print.innerHTML = "Você é libra.";
+            }
+        case 10:
+            if (day <= 22) {
+                print.innerHTML = "Você é libra.";
+            }
+            else if (day >= 23) {
+                print.innerHTML = "Você é escorpião.";
+            }
+        case 11:
+            if (day <= 21) {
+                print.innerHTML = "Você é escorpião.";
+            }
+            else if (day >= 22) {
+                print.innerHTML = "Você é sagitário.";
+            }
+        case 12:
+            if (day <= 21) {
+                print.innerHTML = "Você é sagitário.";
+            }
+            else if (day >= 22) {
+                print.innerHTML = "Você é capricórnio.";
+            }
+    }
+}
+
+
+// Enxergo no programa ONE uma grande oportunidade de aperfeiçoamento e crescimento, uma vez que o projeto tem como propósito capacitar estudantes a partir de tecnologias muito utilizadas no mercado de trabalho mundial e isso é muito instigante. A possibilidade de realizar um curso gratuito tão completo como esse me deixa muito feliz, pois alinha a vontade de aprender com a oportunidade. Portanto, quero muito conseguir participar do programa ONE que já me brilha os olhos desde a pré-inscrição em agosto. Desde já, obrigado pela atenção e pela oportunidade.
