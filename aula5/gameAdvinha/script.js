b02f935c31b77900c6d25b9f3d43f7b2465d5407
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2022-11-23 15:24:22 -0300

    ajustes no script

--- aula5/gameAdvinha/script.js
+++ aula5/gameAdvinha/script.js
@@ -1,7 +1,8 @@
 var resultado = document.getElementsByTagName('p')[1]
 var jogador = Number(document.getElementsByTagName('input')[0].value)
-var input2 = document.getElementsByTagName('input')[1]
-input2.addEventListener('click', sortear)
+var input2 = document.getElementsByTagName('input')[2]
+var input3 = document.getElementsByTagName('input')[1]
+input3.addEventListener('click', sortear)
 input2.addEventListener('click', jogar)
 
 var computador = 0
@@ -15,24 +16,18 @@ function sortear() {
 }
 
 function jogar() {
-    var count = 0
     var jogador = Number(document.getElementsByTagName('input')[0].value)
 
-    count++
-
-    if (jogador == computador) {
-        resultado.innerHTML += `Parabéns! Você descobriu o número. <br>`
-    }
-    else if (jogador > computador) {
-        resultado.innerHTML += `O seu palpite é <b>maior</b> que o número sorteado. <br>`
-    }
-    else if (jogador < computador) {
-        resultado.innerHTML += `O seu palpite é <b>menor</b> que o número sorteado. <br>`
-    }
-    else if (count === 3) {
-        resultado.innerHTML += `Seus palpites acabaram, boa sorte da próxima vez! <br>`
-    }
-    else {
-        resultado.innerHTML += `Valor informado é inválido. <br>`
-    }
+        if (jogador == computador) {
+            resultado.innerHTML += `Parabéns! Você descobriu o número. <br>`
+        }
+        else if (jogador > computador) {
+            resultado.innerHTML += `O seu palpite é <b>maior</b> que o número sorteado. <br>`
+        }
+        else if (jogador < computador) {
+            resultado.innerHTML += `O seu palpite é <b>menor</b> que o número sorteado. <br>`
+        }
+        else {
+            resultado.innerHTML += `Valor informado é inválido. <br>`
+        }
 }
