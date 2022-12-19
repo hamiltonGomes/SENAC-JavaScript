51c4f919bf2244a6db6ec6e023abd108e2f7a2d6
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2022-12-19 14:28:16 -0300

    Delete SENAC-JavaScript directory

--- SENAC-JavaScript/aula1/functionJS/script.js
+++
@@ -1,4 +0,0 @@
-function funcao1() {
-    var nomePessoa = document.forms["formCadastro"]["nome"].value;
-    alert("Olá " + nomePessoa + "!"); 
-} 
--- SENAC-JavaScript/aula12/exemplo1Array/script.js
+++
@@ -1,5 +0,0 @@
-const fruits = ["Banana", "Laranja", "Morango", "Maça", "Açerola"];
-document.getElementById("demo").innerHTML = "A quantidade de frutas é: " + fruits.length;
-document.getElementById("demo1").innerHTML = "A lista de frutas é a seguinte: " + fruits
-
-
