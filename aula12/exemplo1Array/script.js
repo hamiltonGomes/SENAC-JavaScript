90c62fe2c105885b62e5114db00f1af186f90403
Hamilton <110085367+hamiltonGomes@users.noreply.github.com>
2022-12-19 14:31:17 -0300

    Add files via upload

---
+++ aula12/exemplo1Array/script.js
@@ -0,0 +1,5 @@
+const fruits = ["Banana", "Laranja", "Morango", "Maça", "Açerola"];
+document.getElementById("demo").innerHTML = "A quantidade de frutas é: " + fruits.length;
+document.getElementById("demo1").innerHTML = "A lista de frutas é a seguinte: " + fruits
+
+
